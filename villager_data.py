7711b5a52d0a298491831562cac9fc76a64bf710
mara-olson <mara.c.olson@gmail.com>
2022-02-05 17:31:21 -0800

    Completed three functions of the data_structures lab

---
+++ villager_data.py
@@ -0,0 +1,156 @@
+"""Functions to parse a file containing villager data."""
+
+villager_file = open("villagers.csv")
+
+def all_species(filename):
+    """Return a set of unique species in the given file.
+
+    Arguments:
+        - filename (str): the path to a data file
+
+    Return:
+        - set[str]: a set of strings
+    """
+
+    # species = set()
+
+    # TODO: replace this with your code
+    species = []
+    for line in filename:
+        items_in_line = line.split('|')
+        species.append(items_in_line[1])
+
+    return set(species)
+
+# print(all_species(villager_file))
+
+
+
+
+def get_villagers_by_species(filename, search_string="All"):
+    """Return a list of villagers' names by species.
+
+    Arguments:
+        - filename (str): the path to a data file
+        - search_string (str): optional, the name of a species
+
+    Return:
+        - list[str]: a list of names
+    """
+
+    villagers = []
+    for line in filename:
+        items_in_line = line.split('|')
+        # villager_name = line[0]
+        # species = line[1]
+        if items_in_line[1] == search_string:
+            villagers.append(items_in_line[0])
+        elif search_string == 'All':
+            villagers.append(items_in_line[0])
+        elif not items_in_line[1]:
+            villagers.append(items_in_line[0])
+        #assuming user doesn't enter a species that isn't in the list
+
+
+    # TODO: replace this with your code
+
+    return sorted(villagers)
+# print(get_villagers_by_species(villager_file, 'Hamster'))
+
+
+
+
+def all_names_by_hobby(filename):
+    """Return a list of lists containing villagers' names, grouped by hobby.
+
+    Arguments:
+        - filename (str): the path to a data file
+    
+    Return:
+        - list[list[str]]: a list of lists containing names
+    """
+
+    # TODO: replace this with your code
+    file = open(filename)
+
+    fitness = []
+    nature = []
+    education = []
+    music = []
+    fashion = []
+    play = []
+    for line in file: 
+    # line is one long string for each line in the file
+        items_in_line = line.split('|')
+        # items_in_line is a list of all the items delimited by |
+        
+        if items_in_line[3] == 'Fitness':
+            fitness.append(items_in_line[0])
+        elif items_in_line[3] == 'Nature':
+            nature.append(items_in_line[0])
+        elif items_in_line[3] == 'Education':
+            education.append(items_in_line[0])
+        elif items_in_line[3] == 'Music':
+            music.append(items_in_line[0])
+        elif items_in_line[3] == 'Fashion':
+            fashion.append(items_in_line[0])
+        elif items_in_line[3] == 'Play':
+            play.append(items_in_line[0])
+
+    return [sorted(fitness), sorted(nature), sorted(education), sorted(music), sorted(fashion), sorted(play)]
+
+print(all_names_by_hobby("villagers.csv"))
+
+def all_data(filename):
+    """Return all the data in a file.
+
+    Each line in the file is a tuple of (name, species, personality, hobby,
+    saying).
+
+    Arguments:
+        - filename (str): the path to a data file
+
+    Return:
+        - list[tuple[str]]: a list of tuples containing strings
+    """
+
+    all_data = []
+
+    # TODO: replace this with your code
+
+    return all_data
+
+
+def find_motto(filename, villager_name):
+    """Return the villager's motto.
+
+    Return None if you're not able to find a villager with the
+    given name.
+
+    Arguments:
+        - filename (str): the path to a data file
+        - villager_name (str): a villager's name
+
+    Return:
+        - str: the villager's motto or None
+    """
+
+    # TODO: replace this with your code
+
+
+def find_likeminded_villagers(filename, villager_name):
+    """Return a set of villagers with the same personality as the given villager.
+
+    Arguments:
+        - filename (str): the path to a data file
+        - villager_name (str): a villager's name
+    
+    Return:
+        - set[str]: a set of names
+
+    For example:
+        >>> find_likeminded_villagers('villagers.csv', 'Wendy')
+        {'Bella', ..., 'Carmen'}
+    """
+
+    # TODO: replace this with your code
